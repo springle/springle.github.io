0feeab1f05b6424429743c0be2f6fa6d41dc3ea0
Samuel Pringle <15springle@gmail.com>
2016-01-22 20:01:17 -0800

    added to projects and rearranged entries

--- sam.js
+++ sam.js
@@ -17,17 +17,6 @@ var data = {
 	    ],
 	},
     {
-	    start: new Date("2013-08-01"),
-      end: new Date("2014-07-07"),
-	    title: "Associate",
-	    role: "Cashier",
-	    company: "The Medicine Shoppe",
-	    location: "Downingtown, PA",
-	    summary: [
-		"Pulled and stocked medicine, counted pills, helped customers, operated the register, and answered phones.",
-	    ],
-	},
-    {
       start: new Date("2016-01-13"),
       end: new Date("2016-05-13"),
       title: "Lab Assistant",
@@ -38,8 +27,27 @@ var data = {
     "Help students figure out difficult problems on labs/homework, answer questions, assist course staff.",
       ],
   },
+    {
+	    start: new Date("2013-08-01"),
+      end: new Date("2014-07-07"),
+	    title: "Associate",
+	    role: "Cashier",
+	    company: "The Medicine Shoppe",
+	    location: "Downingtown, PA",
+	    summary: [
+		"Pulled and stocked medicine, counted pills, helped customers, operated the register, and answered phones.",
+	    ],
+	},
     ],
     degrees: [
+  {
+	    start: new Date("2015-08-22"),
+	    end: new Date("2019-05-15"),
+	    degree: "BA in Computer Science",
+	    summary: ["Degree Anticipated"],
+	    institution: "The University of California, Berkeley",
+	    location: "Berkeley, CA",
+	},
 	{
 	    start: new Date("2010-08-30"),
 	    end: new Date("2015-06-03"),
@@ -48,20 +56,22 @@ var data = {
 	    summary: ["Summa Cum Laude"],
 	    location: "Downingtown, PA",
 	},
-	{
-	    start: new Date("2015-08-22"),
-	    end: new Date("2019-05-15"),
-	    degree: "BA in Computer Science",
-	    summary: ["Degree Anticipated"],
-	    institution: "The University of California, Berkeley",
-	    location: "Berkeley, CA",
-	},
     ],
     academicExperience: [
 	{
 	    start: new Date("2015-12-26"),
-	    end: new Date("2016-01-15"),
+	    end: new Date("2016-02-15"),
 	    title: "<a href='https://github.com/joechip504/sc2-barcode-scanner' target='_blank'>SC2 Barcode Scanner</a>",
+	},
+  {
+	    start: new Date("2013-12-26"),
+	    end: new Date("2014-01-15"),
+	    title: "<a href='http://ttllmm.herokuapp.com' target='_blank'>Turtles That Look Like McConnell</a>",
+	},
+  {
+	    start: new Date("2015-10-15"),
+	    end: new Date("2015-11-15"),
+	    title: "<a href='https://github.com/springle/PythonTweetMatch' target='_blank'>Tweet Match</a>",
 	},
     ],
 };
